bb3271c3ba94a806f947a0875ed46e843c880701
ananya <ananyasensnr@gamil.com>
2024-12-26 13:51:55 +0530

    CFOUR

---
+++ Cfour.php
@@ -0,0 +1,243 @@
+<html>
+<head>
+<title> CFour </title>
+<link rel="stylesheet" href="CFour.css">
+<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+<script src="https://kit.fontawesome.com/a076d05399.js" crossorigin="anonymous"></script>
+<link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@3.3.7/dist/css/bootstrap.min.css" integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonymous">
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@3.3.7/dist/js/bootstrap.min.js" integrity="sha384-Tc5IQib027qvyjSMfHjOMaLkfuWVxZxUPnCJA7l2mCWNIpG9mGCD8wGNIcPD7Txa" crossorigin="anonymous"></script>
+</head>
+<body>
+	<!---------------------------------------------------------------------- nav bar ----------------------------------------------------------->
+	<div class="container">
+  <nav class="navbar navbar-expand-lg navbar-fixed-top navbar-light bg-light" style=" left: 187px;">
+  <a class="navbar-brand">
+  	<img src="./img/logocfour.png" width="240px" height="50px" style="margin-top:56px; margin-left: -17px;" ></a>
+  	<ul class="nav navbar-nav">
+      <li class="list-item"><a href="#" style="text-decoration: none; color: black;">Financial consolidation</a></li>
+      <li class="list-item"><a href="#" style="text-decoration: none; color: black;">ESEF Reporting</a></li>
+      <li class="list-item"><a href="#" style="text-decoration: none; color: black;">CSRD Reporting</a></li>
+      <li class="list-item"><a href="#" style="text-decoration: none; color: black;">Blog</a></li>
+      <li><button class="btn"> Request Demo</button></li>
+    </ul>
+   </nav>
+</div>
+<!--------------------------------- hero section ------------------------------------------------------------------------------------------------->
+<div class="container" style="margin-top: 148px;">
+	<div class="row">
+		<div class="col-md-6">
+			<h1> Powerful Solution For Financial Reporting</h1>
+			<p> CFOUR delivers seamless group financial<br> consolidation,ESEF compliance,and CSRD <br>readiness.Empower your team with <br>fast, accurate and collaborative tools. </p>
+             <ul style="margin-top: 22px;margin-left: 9px;">
+             <li> Accelerate financial processes</li>
+		     <li>  Meet regulatory requirements</li>
+		     <li> Enhance data accuracy</li></ul> <br><br><br>
+             <button class="btn1">Request Demo</button>
+			 </div>
+       <div class="col-md-6">
+       	     <img class=image src=".\img\heroLaptop.png" width="730px" height="450" >
+			<iframe src="https://player.vimeo.com/video/656562023?h=2bca2c32ae" width="600" height="300" frameborder="0" allow="autoplay; fullscreen; picture-in-picture" allowfullscreen></iframe>
+            <p><a href="https://vimeo.com/656562023">CFOUR-TECH-Ad.mp4</a> from <a href="https://vimeo.com/user160599949">Vlad Kalashnikov</a> on <a href="https://vimeo.com">Vimeo</a>.</p></div>
+	    </div>
+       </div>
+ 	    <div class="container">
+	    	<div class="row">
+	    		<div class="col-md-2"><img src="./img/hero1.png" height="60px" width="130px" style="margin-top: 90px; margin-left: 1px;">
+	    		</div>
+	    		<div class="col-md-2"><img src="./img/hero2.png" height="50px" width="100px" style="margin-top: 96px; margin-left: -52px;">
+	    		</div>
+	    		<div class="col-md-2"><img src="./img/hero3.png" height="90px" width="140px" style="margin-top: 58px; margin-left: -141px;">
+	    		</div>
+	    		<div class="col-md-2"><img src="./img/hero4.png" height="90px" width="90px" style="margin-top: 59px; margin-left: -178px;">
+	    		</div>
+	    		<div class="col-md-2"><img src="./img/hero5.png" height="90px" width="110px" style="margin-top: 66px; margin-left: -264px;">
+	    		</div>
+	    	</div>
+        </div>
+        <!----------------------------------------------------------------- 3 container ------------------------------------------------------>
+         <div class="container" style="margin-top: 200px; margin-bottom: 50px; ">
+             <div class="row">
+
+                <div class="col-md-3">
+         			<img src=".\img\col31.png" height="75px" width="220px" style="margin-left: -23px;">
+         			<h2><b>Automate <br> group financial <br> consolidation<b></h2>
+                     <p>Speeds up financial<br> consolidation from days<br> to hours. Integrates with <br>accounting systems, <br>ERPs, Excel, and BI tools.</p>
+                     <button class="btn2">Learn More</button>
+         		</div>
+         		<div class="col-md-3">
+         			<img src=".\img\col32.png" height="100px" width="200px" style="margin-top:-19">
+         			<h2><b>Streamline esef,<br>uksef reporting<b></h2>
+                     <p>Simplify ESEF reporting<br>with an AI-driven<br>platform that automates<br>tagging,ensures <br>accuracy,and enables<br>seamless collaboration.</p>
+                     <button class="btn2">Learn More</button>
+         		</div>
+         		<div class="col-md-3">
+         			<img src=".\img\col33.png" height="100px" width="200px" style="margin-top:-19px">
+         			<h2><b>Be ready for the<br>CSRD mandate<b></h2>
+                     <p>An intuitive CSRD<br>reporting platform that<br>ensures compliance,<br>streamlines processes,<br>and enhance<br>collaboration.</p>
+                     <button class="btn2">Learn More</button>
+         		</div>
+         	</div>
+         </div>
+         <!--------------------------------------------------------- table------------------------------------------------------------- -->
+         <div>
+         	<h1 class="why">Why we are different</h1>
+         </div>
+
+ <div class="container">
+ 	<div class="grid-container">
+ 	<div class="grid-item1 ">CFOUR</div>
+ 	<div class="grid-item2">Other Financial Solutions</div>
+ 	<div class="grid-item3 "><img src="./img/tick.png" width="55px" height="45px"></div>
+ 	<div class="grid-item4 ">Focused on providing an unparalleled user experience.</div>
+ 	<div class="grid-item5"><img src="./img/croos.png" width="55px" height="45px" ></div>
+ 	<div class="grid-item6">Outdated user experience with basic functionality and limited customization options.</div>
+ 	<div class="grid-item7 "><img src="./img/tick.png" width="55px" height="45px"></div>
+ 	<div class="grid-item8 ">Leverages advanced AI-driven solutions to enhance efficiency, accuracy, and decision making.</div>
+ 	<div class="grid-item9"><img src="./img/croos.png" width="55px" height="45px" ></div>
+ 	<div class="grid-item10">Relies on traditional methods and manual processes, resulting in slower, less accurate outcomes and limited</div>
+ 	<div class="grid-item11"><img src="./img/tick.png" width="55px" height="45px"></div>
+ 	<div class="grid-item12">Cloud-based for easy access and scalability</div>
+ 	<div class="grid-item13"><img src="./img/croos.png" width="55px" height="45px" ></div>
+ 	<div class="grid-item14">May require on-premise installation or have limited scalability</div>
+ 	<div class="grid-item15"><img src="./img/tick.png" width="55px" height="45px"></div>
+ 	<div class="grid-item16">Built-in validation ensures accuracy and compliance</div>
+ 	<div class="grid-item17"><img src="./img/croos.png" width="55px" height="45px" ></div>
+ 	<div class="grid-item18">Lacks integrated validation tools, requiring manual checks and increasing the risk of errors and non-compliance.</div>
+ 	<div class="grid-item19"><img src="./img/tick.png" width="55px" height="45px"></div>
+ 	<div class="grid-item20">Hands-on support during integration and implementation</div>
+ 	<div class="grid-item21"><img src="./img/croos.png" width="55px" height="45px" ></div>
+ 	<div class="grid-item22">May offer limited support or charge extra for implementation</div>	
+ 	<div class="grid-item23"><img src="./img/tick.png" width="55px" height="45px"></div>
+ 	<div class="grid-item24">Collaborative environment for seamless teamwork</div>
+ 	<div class="grid-item25"><img src="./img/croos.png" width="55px" height="45px" ></div>
+ 	<div class="grid-item26">Provides limited collaboration tools, leading to disjointed communication and inefficiencies in team workflows.</div>	
+  </div>
+ </div>
+ <!-- ------------------------------------------------------logo section   -------------------------------------------------------------------------------------------- -->
+ <div class= "container" style=" padding-bottom: 69px;margin-top:75px ;margin-left: 188px; box-shadow: 2px 2px 2px 2px  gray;">
+  <div class="heading">Trusted by CFOs</div>
+ 	 <div class="row">
+ 		
+ 			<img src="./img/scrol11.png" width="200px" height="60px" style="margin-left:67px ;">
+ 	
+            <img src="./img/scrol12.png" width="150px" height="60px"style="margin-left: 97px;">
+ 
+             <img src="./img/scrol13.png" width="163px" height="170px"style="margin-left:86px ;">
+        
+            <img src="./img/scrol14.png" width="180px" height="60px"style="margin-left: 74px;">   
+ </div>
+ <div class="row">
+ 	<img src="./img/scrol21.png" width="180px" height="80px" style="margin-left:81px ;">
+ 	
+            <img src="./img/scrol22.png" width="170px" height="100px"style="margin-left: 81px; margin-top: -15px;">
+ 
+             <img src="./img/scrol23.png" width="142px" height="65px"style="margin-left:99px ;">
+        
+            <img src="./img/scrol24.png" width="141px" height="75px"style="margin-left: 101px;">   
+
+</div>  
+</div>
+<!--   ---------------------------------- -------------------------------------about ------------------------------------------------------------------------------------ -->
+<h1 class=why>ABOUT</h1>
+<div class="container">
+	<div class="row ">
+	  <div class="col-md-7 col-md-6 p" style="width:682px; text-align: justify;"><p>CFOUR solutions are designed by Eunoia, a specialized data and AI solutions provider.
+                               <br><br>Data has the power to transform businesses – and that’s the driving force behind Eunoia’s mission. Our expert team successfully delivered multiple data and AI projects in finance, retail, manufacturing, marine, and gaming industries, helping customers drive business growth.
+                                <br><br>Our collaborations revealed a critical need: CFOs and financial leaders required a comprehensive solution to streamline financial reporting and unlock data insights.
+                                 <br><br>This need gave birth to CFOUR – a suite of products tailored to CFOs and financial teams. CFOUR combines our data management and AI expertise to meet the unique needs of financial leaders.</p></div>	
+	  <div class="col-md-5"><img src="./img/about.png" height="430px" width="469px" style="margin-left:17px"></div>	
+      </div>
+	  <p style="font-family: sans-serif ;font-weight: 550; font-size: 19px; letter-spacing: 0.9px; text-align: center; margin-top: 25px;margin-left: 55px;  color: #464646b3">We ensure a seamless CFOUR implementation and onboarding experience, backed by our dedicated support <br>team, always ready to address questions or issues.</p>
+</div>
+<!-- ------------------------------------------------------------Email sign in ------------------------------------------------------------------------------------------ -->
+<div class="container" style="background: #f1f1f1;margin-left: 202px;margin-top: 46px;padding-top: 35px;padding-bottom: 31px;text-align: center;">
+	<div><h1 style="font-weight: 550;padding-bottom: 20px; color: #444444;">Sign up for our newsletter </h1></div>
+	<div>
+		<label for="email" style="font-family: sans-serif;letter-spacing: 1.5px; color: #797979; padding-bottom: 1px;">Your Email</label><br>  
+<input type="text" id="email" name="email">  <br>
+<button class="btn4" type="submit" value="submit">Subscribe</button>  
+</div>
+</div>
+<!-------------------------------------------------------------------- latest news----------------------------------------------------------------------------------------- -->
+<h1 style="font-weight: 550;margin-top: 78px;margin-bottom: 70px;font-size: 40px; color: #1e4164; text-align: center; text-decoration-line: underline ; text-underline-offset: 18px;">Latest News</h1>
+ <div class="container">
+ 	<div class="row" style="margin-left: -19px;">
+ 		<div class="col-md-3">
+ 			<img src=" ./img/latest1.png" style="border-radius: 6px;margin-left: -72px;width: 362px; height: 180px;margin-top: -74px ; margin-bottom: 101px;">
+ 			<a class="contain" href="https://new.cfour.tech/blog/cfour-comply-now-available-on-microsoft-appsource/">
+ 			CFour Comply Now Available on Microsoft AppSource</a>
+ 			<a class="contain" href="#">November 15,2023</a>
+ 			<a class="contain" href="#">San Gwann,Malta-<br>November 15,2023 - <br>Eunoia Limited,today announced the<br> availability of...</a>
+ 		</div>
+ 		<div class="col-md-3" id="container3">
+ 			<img src="./img/latest2.png">
+ 			<a class="contain" href="#">Navigating the ESRS Maze: EFRAG releases a draft list of ESRS data points<br></a>
+ 			<a class="contain" href="#">October 31,2023<br></a>
+ 			<a class="contain" href="#">Introduction in a bid to foster transparency and comparability in sustainability reporting across Europe,the...</a>
+ 		</div>
+ 		<div class="col-md-3"  id ="container4">
+ 			<img src="./img/latest3.png" >
+ 			<a  class="contain" href="#">Navigating the Recent Changes in CSRD Rules by the EU Commission<br></a>
+ 			<a  class="contain" href="#">October 18,2023<br></a>
+ 			<a class="contain" href="#">Introduction The European Commission's recent announcement to postpone specific aspects of the Corporate Sustainability Reporting...</a>
+ 		</div>
+ 		
+ 	</div>
+ </div>
+ <!----------------------------------------------------------background-img log in------------------------------------------------------------------------------------------->
+ <h1 style="font-weight: 550;margin-top: 78px;margin-bottom: 70px;font-size: 25px; color: #1e4164; text-align: center; text-decoration-line: underline ;text-underline-offset: 18px;">View All</h1>
+ <div class="container-fluid " style=" background-image: url(https://new.cfour.tech/wp-content/uploads/2021/11/bottom.jpg );width: 105%;height: 500px; background-position: center center;background-size:cover; background-repeat: no-repeat; position: relative; top: 0; left: -29;" >
+ 	<img src="./img/viewimg.png" height="60px" width="70px" style="margin-top:111px;margin-left:750px;">
+ 	<h2 class="why2">Would you like to learn more?</h2>
+ 	<p class="why3">Speak with us to see how we can help you with your financial <br>consolidation and/or regulatory reporting requirements.</p>
+     <button class="btn1" style="margin-left:695;margin-top: 22px;">Request Demo</button>
+</div>
+<!--------------------------------------------------------------container blue----------------------------------------------------------------------------------------------->
+<div class="container-fluid" style="background-color: #1e4164; height: 250px;">
+	<div class="container">
+    <img src="./img/cfourwhite.png" style="margin-top: 75px;width: 190px;">
+    <input type="email" placeholder="Email Address" width="200px" id="email2">
+    <button class="btn3">Submit</button>
+
+
+
+<!-- list -->
+<div>
+	<h4 style="font-weight: bold; position: relative;margin-left: 355px;margin-bottom: 19px; color: white; top: -45px;">PRODUCT</h4>
+	<ul style="margin-top: -48px;  margin-left: 315px; list-style: none;color: white;">
+		<li><a href="#" style="text-decoration: none; color: white;">Financial Consolidation</a></li>
+		<li><a href="#" style="text-decoration: none; color: white;">ESEF reporting</a></li>
+		<li><a href="#" style="text-decoration: none; color: white;">CSRD reporting</a></li>
+	</ul>
+</div>
+
+<div>
+	<h4 style="font-weight: bold; position: relative;top: -106px;margin-bottom: 7px;margin-left: 627px; color: white;">COMPANY</h4>
+	<ul style="margin-top: -100px;  margin-left: 590px; list-style: none; color: white; text-decoration: none;">
+		<li><a href="#" style="text-decoration: none;color: white;">About Us</a></li>
+        <li><a href="#" style="text-decoration: none;color: white;">Blog</a></li>
+		<li><a href="#" style="text-decoration: none;color: white;">Careers</a></li>
+		<li><a href="#" style="text-decoration: none;color: white;">Contact</a></li>
+	</ul>
+</div>
+<span><h3 style="font-size: xx-large;font-family: monospace;font-weight: bold;color: white; position: relative; top: -140px; right: -1100px;"><a href="#" style="text-decoration:none; color:white">f</a></h3></span>
+<span><h3 style="font-family: system-ui; color: white; font-weight: bold; position: relative; top: -150px;right: -1100px;"><a href="#" style="text-decoration:none; color:white">in</a></h3></span>
+</div>
+</div>
+<!----------------------------------------------------------------logo 4---------------------------------------------------------------------------------------------------->
+<div class="container-fluid" style="height: 200px">
+	            <img src="./img/hero1.png" height="60px" width="130px" style="margin-top: 90px; margin-left: 199px;">
+	    		<img src="./img/hero3.png" height="90px" width="140px" style="margin-top: 58px; margin-left: 162px;">
+	    	    <img src="./img/hero4.png" height="90px" width="90px" style="margin-top: 59px; margin-left: 163px;">
+	            <img src="./img/hero2.png" height="50px" width="100px" style="margin-top: 67px; margin-left: 197px;">
+</div>
+<!----------------------------------------------------------------footer---------------------------------------------------------------------------------------------------->
+<footer class="container-fluid0" style="background-color:#1e4164;; height: 90px; ">
+  <div class="container" >
+	<p style="margin-top:35px; margin-left:292px;color:black">© 2024 CFOUR. COPYRIGHT CFOUR 2024 - <a href="#" style="text-decoration: none; color: #31a1e1 ;"> TERMS & CONDITIONS </a>|<a href="#" style="text-decoration: none; color: #31a1e1;"> PRIVACY POLICY </a></p>
+</div>
+</footer>
+</body>
+</html>
+ 
+
